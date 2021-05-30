4ae46aebb3760b8b1ead1b436a913eda1750ba35
victor alfonso lopez cortazar <rmatico13@gmail.com>
2021-05-30 13:15:22 -0500

    nueva modificacion

--- CapturaActas/settings.py
+++ CapturaActas/settings.py
@@ -154,3 +154,5 @@ DATABASES['default'].update(db_from_env)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+import django_heroku
+django_heroku.settings(locals())
