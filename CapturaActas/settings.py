145bdf6e36d0686262fdc9d583e20d7981d76bdf
victor alfonso lopez cortazar <rmatico13@gmail.com>
2021-05-29 21:57:53 -0500

    Added files and changes required for deployment to heroku

--- CapturaActas/settings.py
+++ CapturaActas/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,3 +144,8 @@ STATICFILES_DIRS= (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Heroku: Update database configuration from $DATABASE_URL.
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CapturaActas.wsgi --log-file -
