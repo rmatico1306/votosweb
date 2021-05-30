69e709d18ec754685e915d2d965c0b2f5dda8e3d
victor alfonso lopez cortazar <rmatico13@gmail.com>
2021-05-29 21:03:21 -0500

    First version of application moved into github

--- CapturaActas/settings.py
+++ CapturaActas/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z-h(4eo+qy5e4+$v#-0i@&fo051)c2p%z5pbd-r#myp&!_mx($'
+#SECRET_KEY = 'django-insecure-z-h(4eo+qy5e4+$v#-0i@&fo051)c2p%z5pbd-r#myp&!_mx($'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = False
-
+#DEBUG = False
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 ALLOWED_HOSTS = ['*']
 
 
